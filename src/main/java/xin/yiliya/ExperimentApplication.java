0d0bd3b697e6cd91f46de5020ca8e4d81560958a
aiurmaple <31840624+aiurmaple@users.noreply.github.com>
2018-01-11 23:30:40 +0800

    更新

--- src/main/java/xin/yiliya/ExperimentApplication.java
+++ src/main/java/xin/yiliya/ExperimentApplication.java
@@ -3,10 +3,18 @@ package xin.yiliya;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("xin.yiliya.dao")
-public class ExperimentApplication {
+public class ExperimentApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExperimentApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExperimentApplication.class, args);
 	}
