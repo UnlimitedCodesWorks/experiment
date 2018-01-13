329bb45e061c1b4ab91d04239d10ca450d975c21
jiangerHHF <31845468+jiangerHHF@users.noreply.github.com>
2018-01-13 20:38:08 +0800

    javadoc

--- src/main/java/xin/yiliya/service/FriendsService.java
+++ src/main/java/xin/yiliya/service/FriendsService.java
@@ -9,28 +9,32 @@ import java.util.List;
 @Service
 public interface FriendsService {
 
-    //添加好友
-    //参数：Friends对象
-    //成功返回true，若失败返回false
-    //两边都处于待接受状态0
+    /**
+     * 添加好友
+     * @param friends
+     * @return 成功返回true，若失败返回false。两边都处于待接受状态0
+     */
     Boolean addFriend(Friends friends);
 
-    //确认添加为好友
-    //参数：Friends对象
-    //成功返回true，若失败返回false
-    //两边状态变为1
+    /**
+     * 确认添加为好友
+     * @param friends
+     * @return 成功返回true，若失败返回false。两边状态变为1
+     */
     Boolean sureFriend(Friends friends);
 
-    //拒绝添加为好友
-    //参数：Friends对象
-    //成功返回true，若失败返回false
-    //删除添加纪录
+    /**
+     * 拒绝添加为好友
+     * @param friends
+     * @return 成功返回true，删除添加纪录；若失败返回false
+     */
     Boolean refuseFriend(Friends friends);
 
-    //删除好友
-    //参数：Friends对象
-    //返回：如果删除成功为true，不成功为false
-    //两边好友都会删除
+    /**
+     * 删除好友
+     * @param friends
+     * @return 如果删除成功为true，两边好友都会删除；不成功为false
+     */
     Boolean deleteFriend(Friends friends);
 
 }
--- src/main/java/xin/yiliya/service/UserService.java
+++ src/main/java/xin/yiliya/service/UserService.java
@@ -9,29 +9,47 @@ import java.util.List;
 @Service
 public interface UserService {
 
-    //注册接口
-    //注册成功返回id，否则返回0
+    /**
+     * 用户注册
+     * @param user 用户实体类
+     * @return 用户的id，若失败则返回0
+     */
     Integer Register(User user);
 
-    //修改用户信息
-    //修改成功返回id，否则返回0
+    /**
+     *
+     * @param user
+     * @return 修改成功返回id，否则返回0
+     */
     Integer updateInfo(User user);
 
-    //登陆接口
-    //返回User对象
+    /**
+     * 用户登录
+     * @param num 用户账号
+     * @param pass 用户密码
+     * @return 用户的id，若失败则返回0
+     */
     Integer login(String num, String pass);
 
-    //获取主界面信息
-    //返回UserLaunch对象，包含个人信息，好友信息
+    /**
+     * 获取主界面信息
+     * @param userId
+     * @return 返回UserLaunch对象，包含个人信息，好友信息
+     */
     UserLaunch getLaunchInfo(Integer userId);
 
-    //根据输入内容查找用户
-    //返回List对象
+    /**
+     * 根据输入内容查找用户
+     * @param input
+     * @return 返回List User对象
+     */
     List<User> getUsersByInput(String input);
 
-    //好友申请请求
-    //参数：用户id
-    //返回：发来请求的好友List
+    /**
+     * 好友申请请求
+     * @param userId
+     * @return 发来请求的好友List
+     */
     List<User> usersOfRequireList(Integer userId);
 
 }
